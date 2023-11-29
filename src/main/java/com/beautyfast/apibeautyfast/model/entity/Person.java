2966f144829389a6bc74937cc42ce2661f8ad16a
matheusnicolau-tcc <matheusnicolau.dev.tcc@gmail.com>
2023-11-28 22:07:36 -0400

    feat: creating the entities and relationships

--- src/main/java/com/beautyfast/apibeautyfast/model/entity/Person.java
+++ src/main/java/com/beautyfast/apibeautyfast/model/entity/Person.java
@@ -1,7 +1,14 @@
-package com.beautyfast.model.entity;
+package com.beautyfast.apibeautyfast.model.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Person {
 
+    @Column(nullable = false)
     protected String name;
+
+    @Column(nullable = false)
     protected String cpf;
 }
--- src/main/java/com/beautyfast/apibeautyfast/model/entity/User.java
+++ src/main/java/com/beautyfast/apibeautyfast/model/entity/User.java
@@ -1,8 +1,26 @@
-package com.beautyfast.model.entity;
+package com.beautyfast.apibeautyfast.model.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "tb_user")
 public class User extends Person{
+
+    @Id
+    @Column(name = "user_id",nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(name = "user_salary",nullable = false)
+    private BigDecimal salary;
+
+    @Column(name = "user_position",nullable = false)
+    private String userPosition;
 
 }
